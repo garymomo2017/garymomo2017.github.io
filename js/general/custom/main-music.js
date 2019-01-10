64f9f4a0af61e068f3dd17ea4a611015ba01192b
GaryHsu <gary.hsu@thinkpower-info.com>
2019-01-10 22:36:12 +0800

    2019/01/10 版面大改，多點桃色

--- js/general/custom/main-music.js
+++ js/general/custom/main-music.js
@@ -120,6 +120,27 @@
 			location.replace("https://garymomoindex.github.io/?visit=true");
 		}
 	});
+
+	$(".btn-modal").on("click tap", function() {
+		var htmlIframe1 = '<iframe src="https://www.youtube.com/embed/';
+		var htmlIframe2 = '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+		var htmlIframeSrc = '';
+		var chkId = $(this).attr("id");
+		if (chkId == 'adModala2-1') {
+			htmlIframeSrc = '_ia8PnObNr4';
+		} else if (chkId == 'adModala2-2') {
+			htmlIframeSrc = 'Ka2ZmkwaZeQ';
+		} else if (chkId == 'adModalb3') {
+			htmlIframeSrc = 'z7aH2EUNoAs';
+		} else if (chkId == 'adModalb8') {
+			htmlIframeSrc = 'AlzHESrVZQg';
+		}
+		$("#adModal").find("#video-block").append(htmlIframe1 + htmlIframeSrc + htmlIframe2);
+	});
+
+	$("#adModal").on("hide.bs.modal", function() {
+		$(this).find("#video-block").empty();
+	});
 });
 
 $(window).on("load", function() {
